b03d6bd29dd06d5436e1c0703e4027d2487366e9
Florian-Grevin <grevflor@gmail.com>
2026-02-18 10:36:30 +0100

    Specify video and screenshot folders path

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  videosFolder: "cypress/videos",
+  screenshotsFolder: "cypress/screenshots",  
   allowCypressEnv: false,
   projectId: "rdysc9",
   e2e: {
