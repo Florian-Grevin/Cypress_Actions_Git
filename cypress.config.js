b80c3aeb57c847c1e961de78e25aa8551d809d4e
Florian-Grevin <grevflor@gmail.com>
2026-02-18 10:39:31 +0100

    Attempt to save and keep videos

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   videosFolder: "cypress/videos",
+  video: true,
+  videoCompression: 32,
   screenshotsFolder: "cypress/screenshots",  
   allowCypressEnv: false,
   projectId: "rdysc9",
